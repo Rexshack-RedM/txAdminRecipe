d5ae4dffb83e9d9d761cbf236f0d26410e6b2fa4
RexshackGaming <rexshackuk@gmail.com>
2024-11-03 18:52:53 +0000

    add ox_doorlock

--- rsgcore.sql
+++ rsgcore.sql
@@ -150,3 +150,10 @@ CREATE TABLE IF NOT EXISTS `player_jobs` (
     `grade` INT(11) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `ox_doorlock` (
+  `id` int (11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `data` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+);
