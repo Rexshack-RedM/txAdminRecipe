3c68c9aed67e6e313e0ea763a5617df68cfa29ac
RexShack <rexshackuk@gmail.com>
2026-09-04 13:51:41 +0100

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -133,20 +133,22 @@ CREATE TABLE IF NOT EXISTS `player_ammo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `player_weapons` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `serial` varchar(16) NOT NULL,
   `citizenid` varchar(9) NOT NULL,
   `components` varchar(4096) NOT NULL DEFAULT '{}',
   `components_before` varchar(4096) NOT NULL DEFAULT '{}',
+  `price` decimal(5,2) NOT NULL DEFAULT 0.00,
+  `town` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `player_weapons_custom` (
-  `gunsiteid` VARCHAR(20) NOT NULL,
-  `propid` VARCHAR(20) NOT NULL,
-  `citizenid` VARCHAR(50) NOT NULL,
-  `item` VARCHAR(50) NOT NULL,
-  `propdata` LONGTEXT NOT NULL,
+  `gunsiteid` varchar(20) NOT NULL,
+  `propid` varchar(20) NOT NULL,
+  `citizenid` varchar(50) NOT NULL,
+  `item` varchar(50) NOT NULL,
+  `propdata` longtext NOT NULL,
   PRIMARY KEY (`gunsiteid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
