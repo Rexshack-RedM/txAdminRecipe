9a93225ea37dd99e0dd0fad0e94da220a83a652d
RexShack <rexshackuk@gmail.com>
2026-09-23 14:25:45 +0100

    add rsg-goldclaim

--- rsgcore.sql
+++ rsgcore.sql
@@ -506,3 +506,27 @@ CREATE TABLE IF NOT EXISTS `player_samples` (
     UNIQUE KEY `idx_citizenid_sampleid` (`citizenid`,`sample_id`),
     KEY `idx_citizenid` (`citizenid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `player_goldrockers` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `citizenid` varchar(50) DEFAULT NULL,
+    `owner` varchar(50) DEFAULT NULL,
+    `properties` text NOT NULL,
+    `propid` int(11) NOT NULL,
+    `proptype` varchar(50) DEFAULT NULL,
+    `licensed` tinyint(1) NOT NULL DEFAULT 0,
+    `claimname` varchar(100) DEFAULT NULL,
+    `paydirt` int(3) NOT NULL DEFAULT 0,
+    `water` int(3) NOT NULL DEFAULT 0,
+    `quality` int(3) NOT NULL DEFAULT 100,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `player_smelter` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `citizenid` varchar(50) DEFAULT NULL,
+    `properties` text NOT NULL,
+    `propid` int(11) NOT NULL,
+    `proptype` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
